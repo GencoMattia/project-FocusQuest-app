234d4e4070dcaffd00b9802653477701924b46c8
GencoMattia <gencomattia@hotmail.it>
2024-10-03 18:19:35 +0200

    Fixed form API post call

--- src/pages/PageSignIn.vue
+++ src/pages/PageSignIn.vue
@@ -4,45 +4,57 @@ import axios from "axios";
 export default {
     data() {
         return {
-
+            userEmail: "",
+            userName: "",
+            userSurname: "",
+            userPassword: "",
         };
     },
 
+    computed: {
+        signInForm() {
+            return {
+                email: this.userEmail,
+                name: this.userName,
+                surname: this.userSurname,
+                password: this.userPassword
+            };
+        }
+    },
+
     methods: {
-        createNewUser() {
-            axios.post("http://127.0.0.1:8000/api/sign-in")
+        createNewUser(event) {
+            event.preventDefault();  
+            axios.post("http://127.0.0.1:8000/api/sign-in", this.signInForm)
             .then((response) => {
                 console.log(response);
-            })
+            }).catch((error) => {
+                console.log(error.response);
+            }); 
         }
     }
-
 };
 </script>
 
 <template>
-    <form @submit="createNewUser">
+    <form @submit.prevent="createNewUser">
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <input type="email" class="form-control" id="exampleInputEmail1" name="email" v-model="userEmail">
+            <div class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
             <label for="InputName" class="form-label">Name</label>
-            <input type="text" class="form-control" id="InputName" aria-describedby="emailHelp" name="name">
+            <input type="text" class="form-control" id="InputName" name="name" v-model="userName">
         </div>
         <div class="mb-3">
             <label for="InputSurname" class="form-label">Surname</label>
-            <input type="text" class="form-control" id="InputSurname" aria-describedby="emailHelp" name="surname">
+            <input type="text" class="form-control" id="InputSurname" name="surname" v-model="userSurname">
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+            <input type="password" class="form-control" id="exampleInputPassword1" name="password" v-model="userPassword">
         </div>
-        <!-- <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div> -->
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </template>
