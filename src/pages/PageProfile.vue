f9f0b50f75a0408b5cac2aeebe3eb07913a25cac
GencoMattia <gencomattia@hotmail.it>
2024-10-08 17:40:43 +0200

    Created PageProfile component

---
+++ src/pages/PageProfile.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        };
+    },
+
+    props: {
+        user: {
+            type: Object,
+            required: true // Assicurati che l'oggetto user venga passato come prop
+        }
+    },
+};
+</script>
+
+<template>
+    <div>
+        <h1>Profilo di {{ user.name }} {{ user.surname }}</h1>
+    </div>
+</template>
+
+<style scoped></style>
