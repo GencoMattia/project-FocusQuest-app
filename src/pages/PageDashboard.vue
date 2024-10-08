6d125fc2e268c2162ad912b0a4de52aa026cd1dc
GencoMattia <gencomattia@hotmail.it>
2024-10-08 17:40:19 +0200

    Updated Dashboard with link to page profile

--- src/pages/PageDashboard.vue
+++ src/pages/PageDashboard.vue
@@ -1,18 +1,36 @@
 <script>
+import axios from "axios";
+import UserProfileLink from "@/components/UserProfileLink.vue";
+
 export default {
+    components: {
+        UserProfileLink,
+    },
+
     data() {
         return {
-
+            user: {
+                name: '',
+                surname: ''
+            }
         };
+    },
+
+    methods: {
+        async fetchUserData() {
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/api/auth/me');
+                this.user = response.data;
+            } catch (error) {
+                console.error("Errore nel recupero dei dati dell'utente:", error);
+            }
+        }
     }
 };
 </script>
 
 <template>
-    <h1>
-        Sono la Dashboard
-    </h1>
+    <UserProfileLink :user="user" />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
