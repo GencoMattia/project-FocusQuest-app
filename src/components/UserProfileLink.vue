bb346c56f93e78f4b124a1083e1d67bf3c1a8db9
GencoMattia <gencomattia@hotmail.it>
2024-10-08 17:40:31 +0200

    Created Component with link to page profile

---
+++ src/components/UserProfileLink.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
+
+<template>
+    <div>
+        <h1>Benvenuto, {{ user.name }} {{ user.surname }}</h1>
+        <router-link :to="`/profile/${user.name}-${user.surname}`">Vai al mio profilo</router-link>
+    </div>
+</template>
+
+<style scoped></style>
