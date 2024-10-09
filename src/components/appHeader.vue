0fdcd1baba5dee2447b3af4d991185d484d5a8c8
fedtoscano <federicotoscano611@gmail.com>
2024-10-09 18:00:30 +0200

    modified header

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,8 +1,10 @@
 <script>
+import { store } from '@/store';
 export default {
     data() {
         return {
             isAuthenticated: !!localStorage.getItem("token"),
+            store
         };
     },
 
@@ -10,7 +12,7 @@ export default {
         logOutUser() {
             localStorage.removeItem("token");
             this.isAuthenticated = false;
-            this.$router.push({name: 'home'});
+            this.$router.push({ name: 'home' });
         },
 
         checkAuthStatus() {
@@ -26,54 +28,60 @@ export default {
 
     mounted() {
         this.checkAuthStatus();
+        console.log('utente loggato', this.store.loggedUser)
     }
 };
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li v-if="isAuthenticated">
-                                <a href="#" class="dropdown-item" @click.prevent="logOutUser">Logout</a>
-                            </li>
-                            <li v-else>
-                                <router-link class="dropdown-item" :to="{name: 'login'}">Login</router-link>
-                            </li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <router-link class="nav-link" :to="{ name: 'dashboard'}">Dashboard</router-link>
+                                </li>
+                                <li class="nav-item">
+                                    <router-link class="nav-link" :to="{ name: 'user.task.list'}">Lista Task</router-link>
+                                </li>
+                            </ul>
+                            <div class="d-flex align-items-center">
+                                <div class="dropdown">
+                                    <a class="nav-link dropdown-toggle" role="button" id="navbarDropdown" data-bs-toggle="dropdown"
+                                        aria-expanded="false">
+                                        Azioni
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <li v-if="isAuthenticated">
+                                            <a href="#" class="dropdown-item" @click.prevent="logOutUser">Logout</a>
+                                        </li>
+                                        <li v-if="isAuthenticated">
+                                            <router-link class="dropdown-item" :to="`/profile/${this.store.loggedUser.name}-${this.store.loggedUser.surname}`">Profilo</router-link>
+                                        </li>
+                                        <li v-else>
+                                            <router-link class="dropdown-item" :to="{ name: 'login'}">Login</router-link>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
             </div>
         </div>
-    </nav>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.navbar-nav .nav-item {
+    margin-right: 15px;
+}
+</style>
