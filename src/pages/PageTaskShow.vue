c9b59ed7c9e57fa49af6436a45e37ed30fd15938
fedtoscano <federicotoscano611@gmail.com>
2024-10-10 15:38:10 +0200

    draft of the show page

---
+++ src/pages/PageTaskShow.vue
@@ -0,0 +1,32 @@
+<script>
+import { store } from '@/store';
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            store, 
+            task: {}
+        }
+    },
+
+    methods: {
+        getTaskData($id){
+            axios.get(`http://127.0.0.1:8000/api/${$id}}`)
+            .then((response) =>{
+                console.log('task recuperata con successo')
+                this.task = response.data.task
+            })
+        }
+    },
+
+    mounted(){
+        this.getTaskData()
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped></style>
