ebf1b0d74c0523baf5d491663353983a8133d740
GencoMattia <gencomattia@hotmail.it>
2024-10-17 17:12:24 +0200

    Updated PageDashboard Api calls

--- src/pages/PageDashboard.vue
+++ src/pages/PageDashboard.vue
@@ -27,7 +27,7 @@ export default {
     methods: {
         async fetchUserData() {
             try {
-                const response = await axios.get('http://127.0.0.1:8000/api/user');
+                const response = await axios.get('http://127.0.0.1:8000/api/users/show');
                 console.log(response.data.user);
 
                 this.store.loggedUser.name = response.data.user.name;
