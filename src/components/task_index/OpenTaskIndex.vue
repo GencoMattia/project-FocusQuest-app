048c6c0f273968df162946792771549a4889937e
fedtoscano <federicotoscano611@gmail.com>
2024-10-19 17:36:23 +0200

    created components to show single open tasks

---
+++ src/components/task_index/OpenTaskIndex.vue
@@ -0,0 +1,45 @@
+<script>
+import OpenTaskCard from './TaskCards/OpenTaskCard.vue';
+
+export default {
+    props: {
+        tasks: {
+            type: Array, 
+            required: false
+        }
+    },
+    components: {
+        OpenTaskCard
+    },
+    data() {
+        return {
+            open_tasks: []
+        }
+    },
+
+    mounted() {
+        console.log('Queste sono le Task che arrivano da PageTaskIndex:', this.tasks);
+        this.open_tasks = this.tasks;
+
+        console.log('Queste sono le task da mandare ad OpenTaskCard:', this.open_tasks);
+    }
+}
+</script>
+
+<template>
+    <!-- <p v-for="task in tasks" :key="task.id"> {{ task.name }} </p> -->
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+            <h1>Ciao, sono OpenTaskIndex</h1>
+            <OpenTaskCard 
+                v-for="task in open_tasks"
+                :key="task.id"
+                :single_open_task="task"
+            />
+        </div>
+    </div>
+</div>
+</template>
+
+<style scoped></style>
---
+++ src/components/task_index/TaskCards/OpenTaskCard.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    props: {
+        single_open_task: {
+            type: Object, // Corretto da Array a Object
+            required: false,
+        }
+    },
+    data() {
+        return {
+        }
+    },
+
+    mounted() {
+        console.log('Questa è la singola card della task:', this.single_open_task);
+    }
+}
+</script>
+
+<template>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{ single_open_task.name }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{single_open_task.priority.name}}</h6>
+            <p class="card-text">{{ single_open_task.description }}</p>
+
+            <RouterLink 
+            :to="{name: 'tasks.show', params: {id: single_open_task.id}}">
+            Visualizza Task
+            </RouterLink>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
