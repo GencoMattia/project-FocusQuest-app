e7c7f0f2c362eeb3936fa327be09544ec3313a66
GencoMattia <gencomattia@hotmail.it>
2024-10-22 17:18:22 +0200

    Added validations for Creating new Moment

--- src/components/NewMomentForm.vue
+++ src/components/NewMomentForm.vue
@@ -11,46 +11,80 @@ export default {
                 name: '',
                 task_id: 0,
                 message: '',
-                emotion_id: 0, 
+                emotion_id: 0,
                 moments_type_id: 0,
             },
+
+            errors: {},
         }
     },
-    methods:{
-        getFormData(){
+    methods: {
+        validateInput() {
+            this.errors = {};
+
+            // Task's name validator
+            if (!this.momentData.name) {
+                this.errors.name = "Ogni momento deve avere un nome";
+            } else if (this.momentData.name.length < 3 || this.momentData.name.length > 25) {
+                this.errors.name = "Il nome deve essere compreso tra 3 e 25 caratteri";
+            }
+
+            // Task's message validator
+            if (this.momentData.message.length > 150) {
+                this.errors.message = "Il messaggio può essere lungo al massimo 150 caratteri"
+            }
+
+            return Object.keys(this.errors).length === 0;
+        },
+
+        clearValidationMessage(field) {
+            this.errors[field] = "";
+        },
+
+        getFormData() {
             const task_id = this.$route.params.id
 
             axios.get('http://127.0.0.1:8000/api/moments/form-data', {
-                params:{
+                params: {
                     task_id: task_id,
                 }
             })
-            .then((response)=>{
-                console.log(response);
-                this.emotions = response.data.data.emotions
-                this.moment_types = response.data.data.moment_types
-                this.task = response.data.data.task[0]
-                this.momentData.task_id = this.task.id
-            })
+                .then((response) => {
+                    console.log(response);
+                    this.emotions = response.data.data.emotions
+                    this.moment_types = response.data.data.moment_types
+                    this.task = response.data.data.task[0]
+                    this.momentData.task_id = this.task.id
+                })
         },
 
-        submitForm(){
+        submitForm() {
+            if (!this.validateInput()) {
+                return; //Stop the request if the validation fails
+            }
+
             // console.log('Dati pronti per essere inviati', this.momentData)
             axios.post(`http://127.0.0.1:8000/api/moments/tasks/${this.task.id}/create`, {
-                    name: this.momentData.name,
-                    task_id: this.task.id,
-                    message: this.momentData.message,
-                    emotion_id: this.momentData.emotion_id,
-                    moments_type_id: this.momentData.moments_type_id,
+                name: this.momentData.name,
+                task_id: this.task.id,
+                message: this.momentData.message,
+                emotion_id: this.momentData.emotion_id,
+                moments_type_id: this.momentData.moments_type_id,
             })
-            .then((response) =>{
-                console.log('momento creato con successo')
-            })
+                .then((response) => {
+                    console.log('momento creato con successo')
+                }).catch((error) => {
+                    if (error.response && error.response.data) {
+                        this.errors.server = "Email o password errati";
+                    }
+
+                    console.log("Login Error:", error.response.data);
+                });
 
         }
-        
+
     },
-    mounted(){
+    mounted() {
         this.getFormData()
     }
 
@@ -68,14 +102,20 @@ export default {
                     <input type="hidden" name="task_id" :value="task.id">
                     <div class="mb-3">
                         <label for="name">Insert Moment's Name</label>
-                        <input type="text" name="name" id="moment-form-name"
-                        v-model="momentData.name">
+                        <input type="text" name="name" id="moment-form-name" v-model="momentData.name"
+                            @input="clearValidationMessage('name')">
+                        <div v-if="errors.name" class="error-message">
+                            {{ errors.name }}
+                        </div>
                     </div>
 
                     <div class="mb-3">
                         <label for="message">Insert Moment's message</label>
-                        <input type="text" name="message" id="moment-form-message"
-                        v-model="momentData.message">
+                        <input type="text" name="message" id="moment-form-message" v-model="momentData.message"
+                            @input="clearValidationMessage('message')">
+                        <div v-if="errors.message" class="error-message">
+                            {{ errors.message }}
+                        </div>
                     </div>
 
                     <div class="mb-3">
@@ -85,19 +125,21 @@ export default {
 
                     <div class="mb-3">
                         <label for="name">Insert Moment's Emotion</label>
-                        <select name="emotion_id" id="moment-form-emotion"
-                        v-model="momentData.emotion_id">
+                        <select name="emotion_id" id="moment-form-emotion" v-model="momentData.emotion_id">
                             <option v-for="emotion in emotions" :value="emotion.id">{{ emotion.name }}</option>
                         </select>
                     </div>
 
                     <div class="mb-3">
                         <label for="name">Insert Moment's Type</label>
-                        <select name="moments_type_id" id="moment-form-type"
-                        v-model="momentData.moments_type_id">
-                            <option v-for="moment_type in moment_types" :value="moment_type.id">{{ moment_type.name }}</option>
+                        <select name="moments_type_id" id="moment-form-type" v-model="momentData.moments_type_id">
+                            <option v-for="moment_type in moment_types" :value="moment_type.id">{{ moment_type.name }}
+                            </option>
                         </select>
                     </div>
+
+                    <!-- Show generic error -->
+                    <div v-if="errors.server" class="error-message">{{ errors.server }}</div>
 
                     <button>CREATE MOMENT</button>
                 </form>
@@ -106,4 +148,11 @@ export default {
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+.error-message {
+    color: red;
+    font-size: 0.9rem;
+}
+
+</style>
